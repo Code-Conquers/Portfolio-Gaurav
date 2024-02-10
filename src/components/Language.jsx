a759790e79711fbc039bb882212cde61ffc7fed1
Gaurav <gmpaliwal21@gmail.com>
2024-02-10 14:50:48 +0530

    added-lazy-load

--- src/components/Language.jsx
+++ src/components/Language.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import BallCanvas from "../canvas/Ball";
 import { SectionWrapper } from "../hoc";
 import html from "/assets/html-1.svg";
@@ -15,6 +15,16 @@ import reactnative from "/assets/react-native.svg";
 import firebase from "/assets/firebase.svg";
 
 const Tech = () => {
+  const [loading, setLoading] = useState(true);
+  
+  // Simulate loading delay for demonstration purpose
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setLoading(false); // Set loading to false after timeout
+    }, 2000); // Adjust the timeout duration as needed
+    return () => clearTimeout(timeout); // Cleanup on unmount
+  }, []);
+
   const technologies = [
     {
       name: "Flutter",
@@ -60,14 +70,18 @@ const Tech = () => {
 
   return (
     <div className="flex flex-wrap justify-center gap-5 pt-6">
-      {technologies.map((technology) => (
-        <div className="w-20 h-20 md:w-28 md:h-28" key={technology.name}>
-          <BallCanvas icon={technology.icon} />
-          <p className="text-xs md:text-sm text-center text-gray-500 mt-2">
-            {technology.name}
-          </p>
-        </div>
-      ))}
+      {loading ? (
+        <div>Loading...</div>
+      ) : (
+        technologies.map((technology) => (
+          <div className="w-20 h-20 md:w-28 md:h-28" key={technology.name}>
+            <BallCanvas icon={technology.icon} />
+            <p className="text-xs md:text-sm text-center text-gray-500 mt-2">
+              {technology.name}
+            </p>
+          </div>
+        ))
+      )}
     </div>
   );
 };
